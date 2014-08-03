20c2c276d7b96b3105428909364deecf16453d49
Morgante Pell <morgante.pell@morgante.net>
2014-08-02 18:57:46 -0700

    basic model viewer

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,13 @@
-require('knockout');
+function init() {
+	// var viewer = new JSC3D.Viewer(document.getElementById('cv'));
 
-console.log('hi');
+	// viewer.setParameter('SceneUrl',         '/model.stl');
+	// viewer.setParameter('ModelColor',       '#CAA618');
+	// viewer.setParameter('BackgroundColor1', '#E5D7BA');
+	// viewer.setParameter('BackgroundColor2', '#383840');
+	// viewer.setParameter('RenderMode',       'flat');
+
+	// viewer.init();
+}
+
+$(init);
