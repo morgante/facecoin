06cf415b79ed0fc6b0a9aa289e5f5cba0a990a69
Morgante Pell <morgante.pell@morgante.net>
2014-08-02 20:21:16 -0700

    more js work

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,37 @@
 var fp = require('./fp');
 var BTC = require('bitcoinjs-lib');
+var _ = require('lodash');
 
 function Coin(data) {
 	ko.mapping.fromJS(data, {}, this);
+}
+
+var connection = new WebSocket('ws://ws.blockchain.info/inv');
+var subs = {};
+
+connection.onmessage = function(e) {
+	var message = JSON.parse(e.data);
+
+	var data = message.x;
+
+	data.out.forEach(function(out) {
+		if (subs[out.addr] !== undefined) {
+			subs[out.addr](out);
+		}
+	});
+
+	console.log(message);
+};
+
+
+function subscribeToAddress(address, cb) {
+	subs[address] = cb;
+
+	var op = {"op":"addr_sub", "addr": address};
+
+	console.log(address);
+
+	connection.send(JSON.stringify(op));
 }
 
 function initKO() {
@@ -14,6 +43,7 @@ function initKO() {
 
 	var model = {
 		'photo': 1,
+		'page': 'upload',
 		'coins': [],
 		'selectedDenom': 0,
 		uploadPhoto: function() {
@@ -22,19 +52,34 @@ function initKO() {
 			});
 		},
 		goToCoins: function() {
+			model.page('coins');
 			$views.hide();
 			$coinView.show();
 		},
 		denoms: [100, 500, 600],
 		addCoin: function() {
 			var key = BTC.ECKey.makeRandom();
+			var address = key.pub.getAddress().toString();
+
+			address = '15X4BiV7bc5xN2EYPyPagtayHnFWEQ24zr'; // testing
 
 			var coin = model.coins.mappedCreate({
 				"amount": model.selectedDenom(),
-				"address": key.pub.getAddress.toString(),
+				"address": address,
 				"privateKey": key.toWIF(),
-				"full": false
+				"confirmed": false
 			});
+
+			function confirm() {
+				coin.confirmed(true);
+			}
+
+			subscribeToAddress(address, function(data) {
+				console.log('got data', data);
+			});
+
+			// testing
+			setTimeout(confirm, 500);
 
 			return coin;
 		}
@@ -54,6 +99,19 @@ function initKO() {
 	};
 
 	model = ko.mapping.fromJS(model, spec);
+
+	model.canProceed = ko.computed(function() {
+		if (model.page() === 'coins') {
+			if (model.coins().length > 0) {
+				return _.every(model.coins(), function(coin) {
+					console.log(coin.confirmed());
+					return coin.confirmed();
+				});
+			}
+		}
+
+		return false;
+	});
 
 	model.goToCoins();
 
